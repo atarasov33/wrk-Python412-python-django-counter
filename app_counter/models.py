b10e641d69bb9ae565b81b0508487da0980b17e5
Al <atarasov33@yandex.ru>
2025-09-08 12:36:15 +0300

    change models - add is_favorite + functions save

--- app_counter/models.py
+++ app_counter/models.py
@@ -8,9 +8,22 @@ class Counter(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        blank=True,
+        related_name='counters'
     )
     is_favorite = models.BooleanField(default=False)
 
+    def clean(self):
+        super().clean()
+        if self.is_favorite and self.user:
+            Counter.objects.filter(
+                user =self.user,
+                is_favorite =True
+            ).exclude(pk=self.pk).update(is_favorite=False)
+
     def __str__(self):
         return f"id={self.id} value={self.value}"
+
+    def save(self,args, kwargs):
+        self.full_clean()
+        super().save(args,kwargs)
